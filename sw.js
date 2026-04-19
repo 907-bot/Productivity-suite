6a9d5b68a532ebdaa78dc899e43822d4dc66cc46
Abhishek Adari <abhishekadari85@gmail.com>
2026-04-19 23:27:58 +0530

    Enabled Notification system foundations

--- sw.js
+++ sw.js
@@ -8,3 +8,22 @@ self.addEventListener('install', (e) => {
 self.addEventListener('fetch', (e) => {
   e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });
+
+// Notification Logic
+self.addEventListener('notificationclick', (event) => {
+  event.notification.close();
+  event.waitUntil(
+    clients.openWindow('https://907-bot.github.io/Productivity-suite/')
+  );
+});
+
+self.addEventListener('push', (event) => {
+  const data = event.data ? event.data.json() : { title: 'Productivity OS', body: 'New Alert!' };
+  const opts = {
+    body: data.body,
+    icon: 'productivity_suite_hero_1776616434984.png',
+    badge: 'productivity_suite_hero_1776616434984.png',
+    vibrate: [100, 50, 100]
+  };
+  event.waitUntil(self.registration.showNotification(data.title, opts));
+});
